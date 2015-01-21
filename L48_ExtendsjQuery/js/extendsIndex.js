edcb889ce4346354335b151f511d0376d84b5ac2
crixalis <xsy896@gmail.com>
2015-01-21 17:06:13 +0800

    init commit

---
+++ L48_ExtendsjQuery/js/extendsIndex.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Crixalis on 2015/1/21.
+ */
+$(document).ready(function(){
+	$.myjq();
+});
---
+++ L48_ExtendsjQuery/js/myjQuery.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Crixalis on 2015/1/21.
+ */
+$.myjq=function(){
+	alert("Hello myjQuery");
+}
+
+$.fn.myjq=function(){
+   $(this).text("Hello");
+}
