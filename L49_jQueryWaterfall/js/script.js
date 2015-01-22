f6cfd5785c6d7792038379f4fa1db2c0fd8ccc12
crixalis <xsy896@gmail.com>
2015-01-22 11:53:47 +0800

    init commit

--- L49_jQueryWaterfall/js/script.js
+++ L49_jQueryWaterfall/js/script.js
@@ -4,8 +4,17 @@
 $(document).ready(function(){
    $(window).on("load",function(){
        imgLocation();
+	   var dataImg={"data":[{"src":"11.jpg"},{"src":"12.jpg"},{"src":"13.jpg"},{"src":"14.jpg"},{"src":"15.jpg"}]};
        window.onscroll=function(){
-
+			if(scrollside()){
+				$.each(dataImg.data,function(index,value){
+				    var box=$("<div>").addClass("box").appendTo($("#container"));
+					var content=$("<div>").addClass("content").appendTo(box);
+					//console.log("./images/"+$(value).attr("src"));
+					$("<img>").attr("src","./images/"+$(value).attr("src")).appendTo(content);
+				})
+				imgLocation();
+			}
        };
    });
 });
@@ -14,7 +23,7 @@ function scrollside(){
     var box=$(".box");
     var lastboxHeight=box.last().get(0).offsetTop+Math.floor(box.last().height()/2);
     var documentHeight=$(window).width();
-    var scrollHeight=$(window).scrollTop;
+    var scrollHeight=$(window).scrollTop();
     return (lastboxHeight<scrollHeight+documentHeight)?true:false;
 }
 
